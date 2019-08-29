ffba05f6f6c51ddf475c171d61f677036bf07c8c
unknown <shadowind30@gmail.com>
2019-08-29 17:58:04 -0300

    Mejoras importantes en el cambio de fondo segun la hora

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let fecha = document.querySelector("#fecha-placeholder");
-let hora = parseInt(moment().format("h"));
+let hora;
 let nombre = "Carlos";
 
 let titulo = document.querySelector("#titulo")
 
+setInterval(function cambioCont() {
 if ((hora >=19 && hora<23) || (hora >=0 && hora<6)) {
     titulo.innerHTML = "Buenas noches"+" "+nombre;
     document.documentElement.style.setProperty('--imagen', 'url("img/noche.jpg")');
@@ -19,9 +20,85 @@ if (hora >=12 && hora<19){
     document.documentElement.style.setProperty('--imagen', 'url("img/tarde.jpg")');    
 }
 
+},1000);
+
 moment.locale("ES-DO");
 
 setInterval(function updateFecha() {
   fecha.innerHTML = moment().format('dddd')+" "+moment().format('LL')+" "+moment().format('LTS');
 },1000)
 
+
+function formatoHora(hora){
+    switch(hora){
+        case "12:00 am" : return 0;
+        break;
+        case "01:00 am" : return 1;
+        break;
+        case "02:00 am" : return 2;
+        break;
+        case "03:00 am" : return 3;
+        break;
+        case "04:00 am" : return 4;
+        break;
+        case "05:00 am" : return 5;
+        break;
+        case "06:00 am" : return 6;
+        break;
+        case "07:00 am" : return 7;
+        break;
+        case "08:00 am" : return 8;
+        break;
+        case "09:00 am" : return 9;
+        break;
+        case "10:00 am" : return 10;
+        break;
+        case "11:00 am" : return 11;
+        break;
+        case "12:00 pm" : return 12;
+        break;
+        case "01:00 pm" : return 13;
+        break;
+        case "02:00 pm" : return 14;
+        break;
+        case "03:00 pm" : return 15;
+        break;
+        case "04:00 pm" : return 16;
+        break;
+        case "05:00 pm" : return 17;
+        break;
+        case "06:00 pm" : return 18;
+        break;
+        case "07:00 pm" : return 19;
+        break;
+        case "08:00 pm" : return 20;
+        break;
+        case "09:00 pm" : return 21;
+        break;
+        case "10:00 pm" : return 22;
+        break;
+        case "11:00 pm" : return 23;
+        break;
+
+    }
+}
+
+var horaPunto = moment().format("LT").toLowerCase().split("")
+
+if (horaPunto[1]==":"){
+    horaPunto.unshift("0");
+}
+
+
+
+
+horaPunto[3]="0";
+horaPunto[4]="0";
+
+horaPunto = horaPunto.join('');
+console.log(horaPunto);
+setInterval(function punto(){
+   
+},1000)
+
+hora = formatoHora(horaPunto);
