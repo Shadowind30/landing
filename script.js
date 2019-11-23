f05ce1598e63900a6c762a40d0f7d81d3d4f458f
Shadowind30 <shadowind30@gmail.com>
2019-11-23 01:49:54 -0400

    refactorizando script.js

--- script.js
+++ script.js
@@ -1,120 +1,112 @@
-let fecha = document.querySelector("#fecha-placeholder");
-let cajaTexto = document.querySelector("#text-field");
-let titulo = document.querySelector("#titulo");
-let tab = document.querySelector("#titulo-tab");
+moment.locale("ES-DO");
+
+const fecha = document.querySelector('#fecha-placeholder');
+const cajaTexto = document.querySelector('#text-field');
+const titulo = document.querySelector('#titulo');
+const tab = document.querySelector('#titulo-tab');
 
 let hora = 0;
-let nombre = '';
+let nombre = localStorage.nombre === undefined ? 'Usuario' : localStorage.nombre;
+let horaPunto = moment().format("LT").toLowerCase().split('');
 
-validarNombre();
+if (horaPunto[1] === ':'){
+    horaPunto.unshift('0');
+}
+
+horaPunto[3] = '0';
+horaPunto[4] = '0';
+horaPunto = horaPunto.join('');
+hora = formatoHora(horaPunto);
 
 setInterval(function cambioCont() {
-if (hora >=19 || hora<6) {
-    titulo.innerHTML = "Buenas noches"+" "+nombre;
-    tab.innerHTML = "Buenas noches"+" "+nombre;
+if (hora >= 19 || hora < 6) {
+    titulo.innerHTML = 'Buenas noches' + ' ' + nombre;
     document.documentElement.style.setProperty('--imagen', 'url("img/noche.jpg")');
 }
 
 if (hora >=6 && hora<12){
-    titulo.innerHTML = "Buenos dias"+" "+nombre;
-    tab.innerHTML = "Buenas dias"+" "+nombre;
+    titulo.innerHTML = 'Buenos dias' + ' ' + nombre;
     document.documentElement.style.setProperty('--imagen', 'url("img/dia.jpg")');
 }
 
 if (hora >=12 && hora<19){
-    titulo.innerHTML = "Buenas tardes"+" "+nombre;
-    tab.innerHTML = "Buenas tardes"+" "+nombre;
+    titulo.innerHTML = 'Buenas tardes' + ' ' + nombre;
     document.documentElement.style.setProperty('--imagen', 'url("img/tarde.jpg")');   
 }
 
+tab.innerHTML = titulo.innerHTML;
+
 },1000);
 
-moment.locale("ES-DO");
+
 
 setInterval(function updateFecha() {
-  fecha.innerHTML = moment().format('dddd')+" "+moment().format('LL')+" "+moment().format('LTS');
-},1000)
+  fecha.innerHTML = moment().format('dddd')+ ' ' + moment()
+  .format('LL') + ' ' + moment().format('LTS');
+},1000);
 
 
 function formatoHora(hora){
+    let nHora = 0;
     switch(hora){
-        case "12:00 am" : return 0;
+        case "12:00 am" : nHora = 0;
         break;
-        case "01:00 am" : return 1;
+        case "01:00 am" : nHora = 1;
         break;
-        case "02:00 am" : return 2;
+        case "02:00 am" : nHora = 2;
         break;
-        case "03:00 am" : return 3;
+        case "03:00 am" : nHora = 3;
         break;
-        case "04:00 am" : return 4;
+        case "04:00 am" : nHora = 4;
         break;
-        case "05:00 am" : return 5;
+        case "05:00 am" : nHora = 5;
         break;
-        case "06:00 am" : return 6;
+        case "06:00 am" : nHora = 6;
         break;
-        case "07:00 am" : return 7;
+        case "07:00 am" : nHora = 7;
         break;
-        case "08:00 am" : return 8;
+        case "08:00 am" : nHora = 8;
         break;
-        case "09:00 am" : return 9;
+        case "09:00 am" : nHora = 9;
         break;
-        case "10:00 am" : return 10;
+        case "10:00 am" : nHora = 10;
         break;
-        case "11:00 am" : return 11;
+        case "11:00 am" : nHora = 11;
         break;
-        case "12:00 pm" : return 12;
+        case "12:00 pm" : nHora = 12;
         break;
-        case "01:00 pm" : return 13;
+        case "01:00 pm" : nHora = 13;
         break;
-        case "02:00 pm" : return 14;
+        case "02:00 pm" : nHora = 14;
         break;
-        case "03:00 pm" : return 15;
+        case "03:00 pm" : nHora = 15;
         break;
-        case "04:00 pm" : return 16;
+        case "04:00 pm" : nHora = 16;
         break;
-        case "05:00 pm" : return 17;
+        case "05:00 pm" : nHora = 17;
         break;
-        case "06:00 pm" : return 18;
+        case "06:00 pm" : nHora = 18;
         break;
-        case "07:00 pm" : return 19;
+        case "07:00 pm" : nHora = 19;
         break;
-        case "08:00 pm" : return 20;
+        case "08:00 pm" : nHora = 20;
         break;
-        case "09:00 pm" : return 21;
+        case "09:00 pm" : nHora = 21;
         break;
-        case "10:00 pm" : return 22;
+        case "10:00 pm" : nHora = 22;
         break;
-        case "11:00 pm" : return 23;
+        case "11:00 pm" : nHora = 23;
         break;
+        default: nHora = 0;
 
     }
+
+    return nHora;
 }
 
-var horaPunto = moment().format("LT").toLowerCase().split("")
-
-if (horaPunto[1]==":"){
-    horaPunto.unshift("0");
-}
-
-horaPunto[3]="0";
-horaPunto[4]="0";
-
-horaPunto = horaPunto.join('');
-console.log(horaPunto);
-setInterval(function punto(){
-   
-},1000)
-
-hora = formatoHora(horaPunto);
-
-console.clear();
+//console.clear();
 
 function cambiarNombre(){
-    localStorage.setItem('nombre',  cajaTexto.value);
-    nombre=localStorage.nombre;
-}
-
-function validarNombre(){
-    nombre = localStorage.nombre === undefined ? 'Usuario' : localStorage.nombre;
-
+  localStorage.setItem('nombre',  cajaTexto.value);
+  nombre = localStorage.nombre;
 }
