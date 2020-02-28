023d746f51fd65fead74bceaf0dfa744a75749d1
Shadowind30 <shadowind30@gmail.com>
2020-02-28 00:14:09 -0400

    refactorize la funcion formatoHora()

--- script.js
+++ script.js
@@ -32,14 +32,12 @@ if (hora >=6 && hora<12){
 
 if (hora >=12 && hora<19){
     titulo.innerHTML = 'Buenas tardes' + ' ' + nombre;
-    cambiarFondo('img/tarde.jpg');  
+    cambiarFondo('img/tarde.jpg');
 }
 
 tab.innerHTML = titulo.innerHTML;
 
 },1000);
-
-
 
 setInterval(function updateFecha() {
   fecha.innerHTML = moment().format('dddd')+ ' ' + moment()
@@ -48,60 +46,13 @@ setInterval(function updateFecha() {
 
 
 function formatoHora(hora){
-    let nHora = 0;
-    switch(hora){
-        case "12:00 am" : nHora = 0;
-        break;
-        case "01:00 am" : nHora = 1;
-        break;
-        case "02:00 am" : nHora = 2;
-        break;
-        case "03:00 am" : nHora = 3;
-        break;
-        case "04:00 am" : nHora = 4;
-        break;
-        case "05:00 am" : nHora = 5;
-        break;
-        case "06:00 am" : nHora = 6;
-        break;
-        case "07:00 am" : nHora = 7;
-        break;
-        case "08:00 am" : nHora = 8;
-        break;
-        case "09:00 am" : nHora = 9;
-        break;
-        case "10:00 am" : nHora = 10;
-        break;
-        case "11:00 am" : nHora = 11;
-        break;
-        case "12:00 pm" : nHora = 12;
-        break;
-        case "01:00 pm" : nHora = 13;
-        break;
-        case "02:00 pm" : nHora = 14;
-        break;
-        case "03:00 pm" : nHora = 15;
-        break;
-        case "04:00 pm" : nHora = 16;
-        break;
-        case "05:00 pm" : nHora = 17;
-        break;
-        case "06:00 pm" : nHora = 18;
-        break;
-        case "07:00 pm" : nHora = 19;
-        break;
-        case "08:00 pm" : nHora = 20;
-        break;
-        case "09:00 pm" : nHora = 21;
-        break;
-        case "10:00 pm" : nHora = 22;
-        break;
-        case "11:00 pm" : nHora = 23;
-        break;
-        default: nHora = 0;
-
+    let nHora = Number(hora[0] + hora[1]);
+    if (nHora !== 12){
+    nHora = hora.includes('a') ? nHora : nHora + 12;
     }
-
+    else {
+    nHora = hora.includes('p') ? 12 : 0;
+    }
     return nHora;
 }
 
